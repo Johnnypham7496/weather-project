0da7eb0f869bb2ce98131d131b11bc4281e561d3
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-09 17:29:06 -0500

    added context dictionary from data collected and adjusted spacing for function

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -22,32 +22,42 @@ def index(request):
                 city2, API_KEY, current_weather_url, forecast_weather_url)
         else:
             weather_data2, daily_forecasts2 == None, None
+
+        context = {
+            "weather_data1": weather_data1,
+            "daily_forecasts1": daily_forecasts1,
+            "weather_data2": weather_data2,
+            "daily_forecasts2": daily_forecasts2,
+        }
+
+        return render(request, "weather_app/index.html", context)
     else:
         return render(request, "weather_app/index.html")
 
-    def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url):
-        response = requests.get(
-            current_weather_url.format(city, api_key)).json()
-        lat, lon = response['coord']['lat'], response['coord']['lon']
 
-        forecast_response = requests.get(
-            forecast_weather_url.format(lat, lon, api_key)).json()
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url):
+    response = requests.get(
+        current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
 
-        weather_data = {
-            "city": city,
-            "temperature": round(response['main']['temp'], 2),
-            "description": response['weather'][0]['description'],
-            "icon": response['weather'][0]['icon'],
-        }
+    forecast_response = requests.get(
+        forecast_weather_url.format(lat, lon, api_key)).json()
 
-        daily_forecasts = []
-        for daily_data in forecast_response['daily'][:5]:
-            daily_forecasts.append({
-                "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
-                "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
-                "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
-                "description": daily_data['weather'][0]['description'],
-                "icon": daily_data['weather'][0]['icon'],
-            })
+    weather_data = {
+        "city": city,
+        "temperature": round(response['main']['temp'], 2),
+        "description": response['weather'][0]['description'],
+        "icon": response['weather'][0]['icon'],
+    }
 
-        return weather_data, daily_forecasts
+    daily_forecasts = []
+    for daily_data in forecast_response['daily'][:5]:
+        daily_forecasts.append({
+            "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+            "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
+            "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
+            "description": daily_data['weather'][0]['description'],
+            "icon": daily_data['weather'][0]['icon'],
+        })
+
+    return weather_data, daily_forecasts
