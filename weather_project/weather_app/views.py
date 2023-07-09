cd30d9e861f47c3d0590384b18eabc40bb8fbb77
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-09 17:07:07 -0500

    added index.html file within templates/weather_app directory

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -9,3 +9,9 @@ def index(request):
     API_KEY = open('API_KEY', 'r').read()
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_weather_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
+
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, "weather_app/index.html")
