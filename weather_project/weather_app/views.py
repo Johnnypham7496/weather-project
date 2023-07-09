481f565d87452054b74a08f6a27ae88a5fd777c0
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-09 16:04:07 -0500

    added current weather and forecast urls

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -3,5 +3,9 @@ import datetime
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     API_KEY = open('API_KEY', 'r').read()
+    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    forecast_weather_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
