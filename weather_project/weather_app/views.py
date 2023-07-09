5e2a83c0be5821cc699d75133b9119e7b9ea68b2
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-09 17:21:19 -0500

    added fetch_weather_and_forecast function

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -10,8 +10,35 @@ def index(request):
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_weather_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
-
     if request.method == 'POST':
-        pass
+        city1 = request.POST.get('city1')
+        city2 = request.get('city2', None)
     else:
         return render(request, "weather_app/index.html")
+
+    def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url):
+        response = requests.get(
+            current_weather_url.format(city, api_key)).json()
+        lat, lon = response['coord']['lat'], response['coord']['lon']
+
+        forecast_response = requests.get(
+            forecast_weather_url.format(lat, lon, api_key)).json()
+
+        weather_data = {
+            "city": city,
+            "temperature": round(response['main']['temp'], 2),
+            "description": response['weather'][0]['description'],
+            "icon": response['weather'][0]['icon'],
+        }
+
+        daily_forecasts = []
+        for daily_data in forecast_response['daily'][:5]:
+            daily_forecasts.append({
+                "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+                "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
+                "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
+                "description": daily_data['weather'][0]['description'],
+                "icon": daily_data['weather'][0]['icon'],
+            })
+
+        return weather_data, daily_forecasts
