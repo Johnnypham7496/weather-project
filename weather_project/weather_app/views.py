63310ae7ebf918f271d42e3c72580b0a80233e95
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-09 17:24:52 -0500

    added if statements to check for city2 input

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -13,6 +13,15 @@ def index(request):
     if request.method == 'POST':
         city1 = request.POST.get('city1')
         city2 = request.get('city2', None)
+
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(
+            city1, API_KEY, current_weather_url, forecast_weather_url)
+
+        if city2:
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(
+                city2, API_KEY, current_weather_url, forecast_weather_url)
+        else:
+            weather_data2, daily_forecasts2 == None, None
     else:
         return render(request, "weather_app/index.html")
 
